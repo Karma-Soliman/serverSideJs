2f4baeb559f23f1d35d7606bb54d610c33b63c97
Karma Soliman <karma.soliman@epita.fr>
2026-04-22 13:06:45 +0200

    feat: install multer, configure multer, and test

--- BACK/index.js
+++ BACK/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/students", studentRouter);
-
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.send("Server is running ...");
--- BACK/routes/studentsRoute.js
+++ BACK/routes/studentsRoute.js
@@ -1,8 +1,17 @@
 import express from "express"
 import { getAllStudents, getStudentById, createStudent, updateStudent, deleteStudent } from "../controllers/studentsControllers.js"
+import multerConfig from "../middleware/multer-config.js"
+
+
 
 export const studentRouter = express.Router()
 
+studentRouter.post("/signup", multerConfig, (req, res) => {
+  console.log("req.file:", req.file)
+  console.log("req.body:", req.body)
+
+  res.send("login")
+})
 studentRouter.get("/", getAllStudents)
 studentRouter.get("/:id", getStudentById)
 studentRouter.post("/", createStudent)
