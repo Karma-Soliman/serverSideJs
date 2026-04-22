606a4b9d58dcc69efc4fec20e20c8658a13c3d0e
Karma Soliman <karma.soliman@epita.fr>
2026-04-22 13:25:46 +0200

    feat: validate creation with middleware. create the signup route

--- BACK/routes/studentsRoute.js
+++ BACK/routes/studentsRoute.js
@@ -1,20 +1,15 @@
 import express from "express"
 import { getAllStudents, getStudentById, createStudent, updateStudent, deleteStudent } from "../controllers/studentsControllers.js"
 import multerConfig from "../middleware/multer-config.js"
+import { validateStudent } from "../middleware/validateStudent.js"
 
 
 
 export const studentRouter = express.Router()
 
-studentRouter.post("/signup", multerConfig, (req, res) => {
-  console.log("req.file:", req.file)
-  console.log("req.body:", req.body)
-
-  res.send("login")
-})
+studentRouter.post("/signup", validateStudent,createStudent)
 studentRouter.get("/", getAllStudents)
 studentRouter.get("/:id", getStudentById)
-studentRouter.post("/", createStudent)
 studentRouter.put("/:id", updateStudent)
 studentRouter.delete("/:id", deleteStudent)
 
